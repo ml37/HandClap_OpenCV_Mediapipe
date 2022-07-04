786f1fc86c0deca769f7bdf22c05752da7e69a9e
ml37 <richiemu0416@gmail.com>
2022-07-04 14:28:14 +0900

    Create hello py xr.py

---
+++ hello py xr.py
@@ -0,0 +1,11 @@
+
+import xr
+
+# Query the available VR/AR extensions
+available = xr.enumerate_instance_extension_properties()
+
+# Replace with whatever extensions are required for your
+# particular application...
+required = [xr.KHR_OPENGL_ENABLE_EXTENSION_NAME, ]
+for prop in required:
+    assert prop in available
